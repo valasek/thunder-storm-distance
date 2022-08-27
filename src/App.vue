1ca21af22d46313a88234455f1c518840a93ff87
Stanislav Valasek <valasek@gmail.com>
2022-08-27 19:50:10 +0200

    Initial version

---
+++ src/App.vue
@@ -0,0 +1,182 @@
+<template>
+  <div class="container p-4 grid grid-flow-rows justify-items-center">
+    <h1 class="text-3xl font-bold py-4">Jak daleko je bouřka?</h1>
+    <div class="absolute py-6 right-40 cursor-pointer">
+      <svg
+        class="ml-2 w-6 h-6"
+        xmlns="http://www.w3.org/2000/svg"
+        viewBox="0 0 478.703 478.703"
+      >
+        <g>
+          <g>
+            <path
+              d="M454.2,189.101l-33.6-5.7c-3.5-11.3-8-22.2-13.5-32.6l19.8-27.7c8.4-11.8,7.1-27.9-3.2-38.1l-29.8-29.8
+        c-5.6-5.6-13-8.7-20.9-8.7c-6.2,0-12.1,1.9-17.1,5.5l-27.8,19.8c-10.8-5.7-22.1-10.4-33.8-13.9l-5.6-33.2
+        c-2.4-14.3-14.7-24.7-29.2-24.7h-42.1c-14.5,0-26.8,10.4-29.2,24.7l-5.8,34c-11.2,3.5-22.1,8.1-32.5,13.7l-27.5-19.8
+        c-5-3.6-11-5.5-17.2-5.5c-7.9,0-15.4,3.1-20.9,8.7l-29.9,29.8c-10.2,10.2-11.6,26.3-3.2,38.1l20,28.1
+        c-5.5,10.5-9.9,21.4-13.3,32.7l-33.2,5.6c-14.3,2.4-24.7,14.7-24.7,29.2v42.1c0,14.5,10.4,26.8,24.7,29.2l34,5.8
+        c3.5,11.2,8.1,22.1,13.7,32.5l-19.7,27.4c-8.4,11.8-7.1,27.9,3.2,38.1l29.8,29.8c5.6,5.6,13,8.7,20.9,8.7c6.2,0,12.1-1.9,17.1-5.5
+        l28.1-20c10.1,5.3,20.7,9.6,31.6,13l5.6,33.6c2.4,14.3,14.7,24.7,29.2,24.7h42.2c14.5,0,26.8-10.4,29.2-24.7l5.7-33.6
+        c11.3-3.5,22.2-8,32.6-13.5l27.7,19.8c5,3.6,11,5.5,17.2,5.5l0,0c7.9,0,15.3-3.1,20.9-8.7l29.8-29.8c10.2-10.2,11.6-26.3,3.2-38.1
+        l-19.8-27.8c5.5-10.5,10.1-21.4,13.5-32.6l33.6-5.6c14.3-2.4,24.7-14.7,24.7-29.2v-42.1
+        C478.9,203.801,468.5,191.501,454.2,189.101z M451.9,260.401c0,1.3-0.9,2.4-2.2,2.6l-42,7c-5.3,0.9-9.5,4.8-10.8,9.9
+        c-3.8,14.7-9.6,28.8-17.4,41.9c-2.7,4.6-2.5,10.3,0.6,14.7l24.7,34.8c0.7,1,0.6,2.5-0.3,3.4l-29.8,29.8c-0.7,0.7-1.4,0.8-1.9,0.8
+        c-0.6,0-1.1-0.2-1.5-0.5l-34.7-24.7c-4.3-3.1-10.1-3.3-14.7-0.6c-13.1,7.8-27.2,13.6-41.9,17.4c-5.2,1.3-9.1,5.6-9.9,10.8l-7.1,42
+        c-0.2,1.3-1.3,2.2-2.6,2.2h-42.1c-1.3,0-2.4-0.9-2.6-2.2l-7-42c-0.9-5.3-4.8-9.5-9.9-10.8c-14.3-3.7-28.1-9.4-41-16.8
+        c-2.1-1.2-4.5-1.8-6.8-1.8c-2.7,0-5.5,0.8-7.8,2.5l-35,24.9c-0.5,0.3-1,0.5-1.5,0.5c-0.4,0-1.2-0.1-1.9-0.8l-29.8-29.8
+        c-0.9-0.9-1-2.3-0.3-3.4l24.6-34.5c3.1-4.4,3.3-10.2,0.6-14.8c-7.8-13-13.8-27.1-17.6-41.8c-1.4-5.1-5.6-9-10.8-9.9l-42.3-7.2
+        c-1.3-0.2-2.2-1.3-2.2-2.6v-42.1c0-1.3,0.9-2.4,2.2-2.6l41.7-7c5.3-0.9,9.6-4.8,10.9-10c3.7-14.7,9.4-28.9,17.1-42
+        c2.7-4.6,2.4-10.3-0.7-14.6l-24.9-35c-0.7-1-0.6-2.5,0.3-3.4l29.8-29.8c0.7-0.7,1.4-0.8,1.9-0.8c0.6,0,1.1,0.2,1.5,0.5l34.5,24.6
+        c4.4,3.1,10.2,3.3,14.8,0.6c13-7.8,27.1-13.8,41.8-17.6c5.1-1.4,9-5.6,9.9-10.8l7.2-42.3c0.2-1.3,1.3-2.2,2.6-2.2h42.1
+        c1.3,0,2.4,0.9,2.6,2.2l7,41.7c0.9,5.3,4.8,9.6,10,10.9c15.1,3.8,29.5,9.7,42.9,17.6c4.6,2.7,10.3,2.5,14.7-0.6l34.5-24.8
+        c0.5-0.3,1-0.5,1.5-0.5c0.4,0,1.2,0.1,1.9,0.8l29.8,29.8c0.9,0.9,1,2.3,0.3,3.4l-24.7,34.7c-3.1,4.3-3.3,10.1-0.6,14.7
+        c7.8,13.1,13.6,27.2,17.4,41.9c1.3,5.2,5.6,9.1,10.8,9.9l42,7.1c1.3,0.2,2.2,1.3,2.2,2.6v42.1H451.9z"
+            />
+            <path
+              d="M239.4,136.001c-57,0-103.3,46.3-103.3,103.3s46.3,103.3,103.3,103.3s103.3-46.3,103.3-103.3S296.4,136.001,239.4,136.001
+        z M239.4,315.601c-42.1,0-76.3-34.2-76.3-76.3s34.2-76.3,76.3-76.3s76.3,34.2,76.3,76.3S281.5,315.601,239.4,315.601z"
+            />
+          </g>
+        </g>
+      </svg>
+    </div>
+    <div class="pb-4 pt-4">
+      <button
+        type="button"
+        @click="toggleTimer"
+        class="text-gray-900 bg-[#F7BE38] hover:bg-[#F7BE38]/90 focus:ring-4 focus:outline-none focus:ring-[#F7BE38]/50 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-[#F7BE38]/50 mr-2 mb-2"
+      >
+        {{ actionButtonText }}
+        <svg
+          class="ml-2 w-5 h-5"
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 186.399 186.399"
+          aria-hidden="true"
+          focusable="false"
+          role="img"
+        >
+          <g>
+            <g>
+              <path
+                style="fill: #010002"
+                d="M145.86,30.849C139.195,12.322,121.76,0,101.92,0C83.28,0,66.495,11.152,59.132,28.08
+			c-3.371-1.51-7.029-2.297-10.734-2.297c-14.488,0-26.284,11.731-26.409,26.195c-7.471,5.782-11.886,14.702-11.886,24.19
+			c0,16.815,13.712,30.491,31.01,30.497c0.197,0,0.388,0.006,0.573,0.006s0.346,0,0.465-0.012h28.146
+			c1.641,0,2.983-1.337,2.983-2.983s-1.343-2.983-2.983-2.983H41.995H40.67c-13.563,0-24.601-11.003-24.601-24.524
+			c0-8.079,3.986-15.657,10.663-20.263l1.366-1.283l-0.143-2.434c0-11.271,9.171-20.437,20.442-20.437
+			c3.777,0,7.482,1.062,10.716,3.067l3.162,1.963l1.223-3.509c5.71-16.325,21.153-27.298,38.415-27.298
+			c17.829,0,33.438,11.415,38.832,28.402l0.561,1.748l1.814,0.292c15.764,2.548,27.209,15.985,27.209,31.947
+			c0,17.829-14.547,32.334-32.412,32.334h-21.815c-1.641,0-2.983,1.337-2.983,2.983s1.343,2.983,2.983,2.983h21.815
+			c21.164,0,38.379-17.179,38.379-38.301C176.291,50.062,163.6,34.566,145.86,30.849z"
+              />
+              <path
+                style="fill: #010002"
+                d="M78.148,123.711l24.064-40.342c0.841-1.414,0.37-3.246-1.044-4.093
+			c-1.402-0.841-3.246-0.388-4.099,1.032l-29.447,49.364h40.611L81.185,175.02l-3.192-7.942c-0.621-1.528-2.363-2.261-3.878-1.653
+			c-1.533,0.615-2.273,2.357-1.653,3.884l6.886,17.089l25.037-15.681c1.39-0.871,1.826-2.715,0.943-4.111
+			c-0.871-1.396-2.709-1.814-4.105-0.949l-11.97,7.488l29.494-49.453H78.142v0.018H78.148z"
+              />
+            </g>
+          </g>
+        </svg>
+      </button>
+    </div>
+    <hr />
+
+    <div class="pb-8 text-3xl">
+      {{ meassurement }}
+    </div>
+    <hr />
+    <button
+      @click="showHelpText = !showHelpText"
+      type="button"
+      class="text-gray-900 bg-gray-100 hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center inline-flex items-center dark:focus:ring-gray-500 mr-2 mb-2"
+    >
+      Jak se vzdálenost počítá?
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        class="ml-2 w-5 h-5"
+        viewBox="0 0 64 64"
+      >
+        <g>
+          <path
+            d="M47.546,25.627L32,42.087l-15.546-16.46c-0.758-0.804-2.024-0.839-2.827-0.081c-0.803,0.758-0.839,2.024-0.081,2.827l17,18
+		C30.924,46.773,31.45,47,32,47s1.076-0.227,1.454-0.627l17-18c0.758-0.803,0.722-2.069-0.081-2.827
+		C49.57,24.788,48.305,24.824,47.546,25.627z"
+          />
+        </g>
+      </svg>
+    </button>
+    <div class="mx-20 px-8 py-8 rounded-lg bg-yellow-50" v-if="showHelpText">
+      <p class="pb-2">
+        Základní výpočet vzdálenosti vychází z rychlosti zvuku (v) ve vzduchu
+        při dané teplotě (t):
+      </p>
+
+      <p class="pb-2"><i>v = (331,82 + 0.61 * t) m/s</i></p>
+
+      <p class="pb-2">
+        Rychlost zvuku je tedy přibližně 345m za sekundu při teplotě vzduchu
+        20°C.
+      </p>
+
+      <p class="pb-2">
+        Pro hrubý výpočet <b>první metodou</b> můžeme s pozorováním blesku
+        spustit stopky (nebo počítat sekundy) a při zaslechnutí hromu měření
+        času (počítání) ukončit. Tento časový rozdíl mezi bleskem a hromem v
+        sekundách vynásobíme * 350 a máme přibližnou vzdálenost pozorovatele od
+        blesku v metrech.
+      </p>
+
+      <p class="pb-2">
+        Pro hrubý výpočet <b>druhou metodou</b> můžeme s pozorováním blesku
+        spustit stopky (nebo počítat sekundy) a při zaslechnutí hromu měření
+        času (počítání) ukončit. Tento časový rozdíl mezi bleskem a hromem v
+        sekundách vydělíme / 3 a máme přibližnou vzdálenost pozorovatele od
+        blesku v kilometrech.
+      </p>
+
+      <p class="pb-2">
+        Přesnější údaje jsou v tabulce pod článkem dle aktuální teploty vzduchu.
+        Roli hraje také vlhkost vzduchu. Se stoupající vlhkostí vzduchu roste i
+        rychlost šíření zvuku. Podobně kapky deště umožní větší rychlost šíření
+        zvuku.
+      </p>
+
+      <p>
+        Blesky probíhají jak mezi oblaky (mraky) samotnými, tak mezi oblakem a
+        zemí. Hrom tedy mohl mít původ v blesku vzdáleného někde v horizontu
+        (vodorovně) od nás, stejně ta se může nacházet blesk na námi kdy proběhl
+        mezi dvěma oblaky.
+      </p>
+    </div>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+const isRunning = ref(false);
+const startTime = ref();
+const showHelpText = ref(false);
+const start = "Klikni když vidíš blesk";
+const end = "Klikni když slyšíš hrom";
+const unit = "km";
+const actionButtonText = ref(start);
+const meassurement = ref("Klikni když uvidíš blesk a pak když uslyšíš hrom");
+
+function toggleTimer() {
+  if (isRunning.value === true) {
+    const endTime = new Date();
+    const diff = (endTime.getTime() - startTime.value.getTime()) / 1000;
+    isRunning.value = false;
+    actionButtonText.value = start;
+    meassurement.value = ((diff * 344) / 1000).toFixed(1) + " " + unit;
+  } else {
+    startTime.value = new Date();
+    isRunning.value = true;
+    actionButtonText.value = end;
+  }
+}
+</script>
+
+<style scoped></style>
